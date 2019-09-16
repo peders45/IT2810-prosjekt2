60c3f62bfe41d0ba9ddd9935aea8497aac700211
Jonathan <hermansenjonathan@gmail.com>
2019-09-16 15:27:25 +0200

    Merge fixes

--- prosjekt-2-app/src/components/atoms/SoundPlayer/SoundPlayer.js
+++ prosjekt-2-app/src/components/atoms/SoundPlayer/SoundPlayer.js
@@ -4,11 +4,7 @@ const SoundPlayer = () => {
     return(
         <div>
           <audio controls autoPlay preload="auto">
-<<<<<<< HEAD
             <source src="coolsound.mp3" type="audio/mpeg" />
-=======
-            <source src={selectedSound} type="audio/waw" />
->>>>>>> 9f7e92b53775f3e065bbdc4e42cb02bd25b75b5b
             Your browser does not support the audio element.
           </audio>
         </div>
