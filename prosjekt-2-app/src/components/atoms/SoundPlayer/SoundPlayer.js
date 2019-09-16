02ff33f9049bbc8921913ecf0bfadf29951087d9
Jonathan <hermansenjonathan@gmail.com>
2019-09-16 14:47:24 +0200

    #3 Added text area and full support for ajax fetching of json text #18

--- prosjekt-2-app/src/components/atoms/SoundPlayer/SoundPlayer.js
+++ prosjekt-2-app/src/components/atoms/SoundPlayer/SoundPlayer.js
@@ -4,7 +4,7 @@ const SoundPlayer = ({selectedSound}) => {
     return(
         <div>
           <audio controls autoPlay preload="auto">
-            <source src='${selectedSound}' type="audio/waw" />
+            <source src={selectedSound} type="audio/waw" />
             Your browser does not support the audio element.
           </audio>
         </div>
--- prosjekt-2-app/src/components/atoms/TextArea/TextArea.js
+++ prosjekt-2-app/src/components/atoms/TextArea/TextArea.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const TextArea = ({text}) => {
-    const textSection = JSON.parse(text);
+const TextArea = ({selectedText, selectedTab}) => {
+    const [textSection, setTextSection] = useState([]);
+    const url = 'media/text/'+selectedTab+'/'+selectedText+'.json'
+
+    useEffect(() => {
+        getTextSection();
+    }, [selectedText, selectedTab]);
+  
+    async function getTextSection() {
+      const response = await fetch(url);
+      const textSection = await response.json();
+      setTextSection(textSection);
+    }
+
     return(
         <div>
-        {textSection.text.sections.map((section, i)=>{
+        {textSection.text ? textSection.text.sections.map((section, i)=>{
             return(
             <p key={'S' + i}>{section}</p>
             )
-        })}
+        }) : <p >Text is loading...</p>}
         </div>
     );
 }
