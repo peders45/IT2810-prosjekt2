7142a285979a1b338526808d34165339a1ec36f6
Kristoffer <peders92@hotmail.com>
2019-09-19 11:49:13 +0200

    #14 Add full support for image component

--- prosjekt-2-app/src/components/organisms/DisplaySection/DisplaySection.js
+++ prosjekt-2-app/src/components/organisms/DisplaySection/DisplaySection.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import SoundPlayer from '../../atoms/SoundPlayer/SoundPlayer';
 import TextArea from '../../atoms/TextArea/TextArea'
+import ImageBox from '../../atoms/ImageBox/ImageBox'
 import './DisplaySection.css';
 
-const DisplaySection = ({selectedTab, selectedText}) => {
+const DisplaySection = ({selectedTab, selectedText, selectedImage}) => {
     return(
         <div className="DisplaySection-container">
             <h1>{selectedTab}</h1>
             <div className="text-sound">
               <SoundPlayer />
               <TextArea selectedText={selectedText} selectedTab={selectedTab}/>
+              <ImageBox selectedImage={selectedImage} selectedTab={selectedTab}/>
             </div>
         </div>
     );
