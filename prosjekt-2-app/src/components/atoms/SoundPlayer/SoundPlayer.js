6c9e94f19cbc1da1856b9e59d5e116919c87c0ad
Jonathan <hermansenjonathan@gmail.com>
2019-09-24 12:13:37 +0200

    changed folder name #22

--- prosjekt-2-app/src/components/atoms/SoundPlayer/SoundPlayer.js
+++ prosjekt-2-app/src/components/atoms/SoundPlayer/SoundPlayer.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 const SoundPlayer = ({selectedSound, selectedTab}) => {
   useEffect(() => {
-    document.getElementById('audioPlayer').setAttribute('src', 'media/wav/'+ selectedTab + '/' + selectedSound + '.mp3');
+    document.getElementById('audioPlayer').setAttribute('src', 'media/sound/'+ selectedTab + '/' + selectedSound + '.mp3');
   }, [selectedSound, selectedTab])
   
     return(
         <div>
           <audio id='audioPlayer' controls autoPlay preload="auto">
-            <source src={ 'media/wav/'+ selectedTab + '/' + selectedSound + '.mp3'} type="audio/mpeg" />
+            <source src={ 'media/sound/'+ selectedTab + '/' + selectedSound + '.mp3'} type="audio/mpeg" />
             Your browser does not support the audio element.
           </audio>
         </div>
